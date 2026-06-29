bd917aea43c857283915f8f629534d687c49022e
varshithreddy1109 <varshithreddy1206@gmail.com>
2026-06-29 20:19:06 +0530

    Learned variables in Python

---
+++ variables.py
@@ -0,0 +1,10 @@
+print("I", "AM","LEARNING","PYTHON", sep=' ')
+#sep=' ' 
+print("varshith is king" * 10)#here " "is considered as a string and when we multiply a string with a number then the string will be printed that many times
+st="varshith " 
+st1=st*100
+print(st1)
+price=10 #integer
+rate=4.9 #float
+name="king" #string
+is_published=False #boolean t and f in true and false should be capitial other wise it will result in a error 
