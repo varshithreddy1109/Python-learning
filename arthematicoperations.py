dd1c0aa12493d9345a0d2a1edc457a56666672af
varshithreddy1109 <varshithreddy1206@gmail.com>
2026-06-30 10:43:02 +0530

    learned arthematic operations

---
+++ arthematicoperations.py
@@ -0,0 +1,14 @@
+print(10 + 3)
+print(10 - 3)
+print(10 / 3)#division with decimal values
+print(10 // 3)#interger value of division
+print(10 % 3)#give the remainder
+print(10 ** 3)#power 10power 3
+
+x=10
+x=x+3#  x=x+3 is equivalent to x+=3
+print(x)
+
+x=10
+x-=3 #x = x - 3 isa equivalent to x-=3
+print(x)
